a6ed548f5a45a7760a4d50288b3f4325f1d107c3
Sathvik Nair <sathviknair@berkeley.edu>
2019-11-01 16:48:04 -0700

    run high and low level learning rates

---
+++ code/Hierarchical-SP/hyperparam_sweep.py
@@ -0,0 +1,18 @@
+import os
+import matplotlib.pyplot as plt
+
+#List of processes where we adjust lower level learning rate on the toy dataset
+
+learning_rates = [0.0005, 0.001, 0.002]
+
+cmd = "python run.py --train --training_stage=0 --toy_data"
+
+for hlr in learning_rates:
+    for llr in learning_rates:
+        cmd_with_lrs = cmd + " --high_level_lr " + hlr + " --low_level_lr " + llr
+        os.system(cmd_with_lrs)
+#Read from a text file with results (overall accuracy for now)
+#TODO: modify the code where they log values in the console to also put these values in a text file?
+#Also check what's in model checkpoings/if they themselves log model performance
+
+#Make a plot
