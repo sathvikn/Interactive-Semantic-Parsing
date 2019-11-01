fa5afdc5dd708b14169690a023ae4636e4d8a29a
Sathvik Nair <sathviknair@berkeley.edu>
2019-10-31 18:47:35 -0700

    initial commit

---
+++ hyperparam_sweep.py
@@ -0,0 +1,12 @@
+import os
+import matplotlib.pyplot as plt
+
+#List of processes where we adjust lower level learning rate on the toy dataset
+
+#Subprocess to parallelize runs
+
+#Read from a text file with results (overall accuracy for now)
+#TODO: modify the code where they log values in the console to also put these values in a text file?
+#Also check what's in model checkpoings/if they themselves log model performance
+
+#Make a plot
