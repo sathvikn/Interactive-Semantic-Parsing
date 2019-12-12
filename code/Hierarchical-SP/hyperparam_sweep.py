d5a42dc751ab20db6090ebd2024fa36f245446de
chess123 <kesav@viswanadha.net>
2019-12-12 00:01:04 -0800

    Jupyter notebook to plot values

--- code/Hierarchical-SP/hyperparam_sweep.py
+++ code/Hierarchical-SP/hyperparam_sweep.py
@@ -2,9 +2,15 @@ import os
 import re
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 #List of processes where we adjust lower level learning rate on the toy dataset
+
+def plot_values(data, llr, hlr):
+    keys = sorted(data.keys())
+    with open(str(llr) + "_" + str(hlr) + ".txt", 'w') as f:
+        for k in keys:
+            f.write(str(k) + "\t" + str(data[k][2]) + "\n")
 
 learning_rates = [0.0005, 0.001, 0.002]
 
@@ -33,15 +39,20 @@ dirs = os.listdir(ckpt_dir)
 #     print(data)
 
 for d in dirs:
-    result_dev_performance = ckpt_dir + "/" + d + "/dev_iter2performance.pkl"
-    data = pickle.load(open(result_dev_performance))
-    ###Read data from pickle file
-    #accuracy = []
-    iters = np.arange(len(accuracy)) * 250
+    result_dev_performance = os.path.join(ckpt_dir, d, "dev_iter2performance.pkl")
     matches = re.search('{(.*)_0', d)
     llr = float(matches.group(1))
     matches = re.search('0_(.*)}', d)
     hlr = float(matches.group(1))
-    plt.plot(iters, accuracy, label = "llr = " + str(llr) + ", hlr = " + str(hlr))
-plt.legend()
-plt.show()
+    with open(result_dev_performance) as f:
+        print(f)
+        data = pickle.load(f)
+    ###Read data from pickle file
+        # print(data)
+        plot_values(data, llr, hlr)
+    continue
+    #accuracy = []
+    iters = np.arange(len(accuracy)) * 250
+    # plt.plot(iters, accuracy, label = "llr = " + str(llr) + ", hlr = " + str(hlr))
+# plt.legend()
+# plt.show()
